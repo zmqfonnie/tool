bed00b9827c7043958cc5203a0a39ea8d47f4eea
zmqfonnie <2831412977@qq.com>
2021-07-30 10:41:11 +0800

    璋冭瘯

--- tests/Test.php
+++ tests/Test.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace  Tests;
-require __DIR__.'/../vendor/autoload.php';
+namespace Tests;
+require __DIR__ . '/../vendor/autoload.php';
 
 use Fonnie\Util\Random;
 use GuzzleHttp\Client;
@@ -13,43 +13,28 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
-    public function testTest()
+    public function testRandom()
     {
-
-
-        $stack = [];
-        $this->assertEquals(0, count($stack));
-        array_push($stack, 'foo');
-        // 添加日志文件,如果没有安装monolog，则有关monolog的代码都可以注释掉
-        $this->Log()->error('hello', $stack);
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-
-//        $this->assertEquals('《三体》', \Fonnie\Util\Random::alnum());
-    }
-
-    public function testChange(){
-
-        $this->assertEquals(true, 123);
+        $this->log();
+        $this->assertEquals(true, Random::alnum());
     }
 
 
-    public function Log()
+
+
+
+    public function log($data = [], $msg = '')
     {
 
         // create a log channel
-        echo __DIR__ . '/logs/'.__CLASS__.'.log';
+        echo __DIR__ . '/logs/' . __CLASS__ . '.log';
         $logger = new \Monolog\Logger('日志实例标识');
 
-        $stream_handler = new StreamHandler(__DIR__ . '/logs/'.__CLASS__.'.log', Logger::INFO);
+        $stream_handler = new StreamHandler(__DIR__ . '/logs/' . __CLASS__. '.log', Logger::INFO);
         $stream_handler->setFormatter(new JsonFormatter());
         $logger->pushHandler($stream_handler);
 
-        $logger->error("Error");
-
-        $logger->info('Adding a new user', ['username' => new Random()]);
+        $logger->info($msg, $data);
         return $logger;
 
     }
